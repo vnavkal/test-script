6f340768e205a1b5f850953c4be20929ca2171ac
Viraj <vnavkal0@gmail.com>
2014-05-07 18:35:31 -0700

    Test script changes

--- test_script.rb
+++ test_script.rb
@@ -952,6 +952,47 @@ m = UpstartNetwork::LoanPricing::LoanModel.new(l)
 i = m.get_interest_rate(l.raising_amount)
 {'interest rate' => i.interest_rate_percent, 'apr' => i.apr}
 
+# Candidate 23: 17734
+l = UpstartNetwork::LoanPricingData.new_from_upstart(u, UpstartNetwork::LoanTerms.new(FundingTermSetTemplate.template_loan))
+
+c = CodingBootcamp.where(use_for_pricing: true).first
+l.coding_bootcamp = nil
+
+l.raising_amount = 10000
+l.use_of_funds = UpstartNetwork::UseOfFunds::OTHER
+
+l.student_sat_1600 = nil
+l.school_sat_1600 = 
+l.college_gpa_4 = 3.2
+l.undergrad_bb_major = :'social sciences'
+l.college_grad_year = 2013
+
+job1 = UpstartNetwork::LoanPricing::Job.new(60000.0 / 12.0, Date.new(2011, 9, 1), nil, true)
+l.jobs = []
+
+job_offer = UpstartNetwork::LoanPricing::JobOffer.new(60000 / 12.0, Date.new(2014, 6, 1))
+l.job_offers = [job_offer]
+
+l.number_of_dependents = 0
+l.total_monthly_debt_obligations = 145
+l.home_ownership.monthly_price = 0
+l.home_ownership.ownership_type = "none"
+
+l.credit_score = 650
+l.credit_history_month = 70
+l.delinquencies_in_2_years = 0
+l.open_credit_lines = 4
+l.total_credit_lines_count = 7
+l.revolving_credit_accounts_balance = 7632
+l.revolving_credit_utilized_percent = 69
+l.recent_credit_inquiries = 0
+
+l.deferral_months = 0
+
+m = UpstartNetwork::LoanPricing::LoanModel.new(l)
+i = m.get_interest_rate(l.raising_amount)
+{'interest rate' => i.interest_rate_percent, 'apr' => i.apr}
+
 # Playing around
 l = UpstartNetwork::LoanPricingData.new_from_upstart(u, UpstartNetwork::LoanTerms.new(FundingTermSetTemplate.template_loan))
 
